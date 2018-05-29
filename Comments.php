fb8e36ae436f0edf3f9f0037d93a5a9894e6d694
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-05-29 14:35:18 +0000

    Added Comments-class.

---
+++ Comments.php
@@ -0,0 +1,14 @@
+<?php
+class Comments {
+    
+    
+    
+}
+
+
+$kirby->set('route', array(
+  'pattern' => 'my/awesome/url',
+  'action'  => function() {
+    // do something here when the URL matches the pattern above
+  }
+));
