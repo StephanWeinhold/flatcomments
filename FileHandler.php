e35bc741ebf9369dd34a81abeb75922d8a48d8a7
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-05-30 21:51:11 +0200

    Added logic to append data to file.

--- FileHandler.php
+++ FileHandler.php
@@ -1,2 +1,12 @@
-readFile
-writeFile
+<?php
+class FileHandler {
+
+    public function writeFile($filePath, $json) {
+        if (!file_put_contents($filePath, $json.PHP_EOL , FILE_APPEND | LOCK_EX)) {
+            throw new Exception('JSON couldn\'t be written to file.', 500);
+        }
+    }
+    
+    //readFile
+
+}
