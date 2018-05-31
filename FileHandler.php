1b0d85f14edbfa7c4fe603b4c7fa64c57d017e65
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-05-31 07:27:53 +0200

    Added file reading functionality.

--- FileHandler.php
+++ FileHandler.php
@@ -1,12 +1,20 @@
 <?php
 class FileHandler {
 
+    public function readFile($filePath) {
+        $lines = file_get_contents($filePath);
+        
+        if ($lines === false) {
+            throw new Exception('JSON file couldn\'t be read.', 500);
+        }
+        
+        return $lines;
+    }
+
     public function writeFile($filePath, $json) {
         if (!file_put_contents($filePath, $json.PHP_EOL , FILE_APPEND | LOCK_EX)) {
             throw new Exception('JSON couldn\'t be written to file.', 500);
         }
     }
-    
-    //readFile
 
 }
