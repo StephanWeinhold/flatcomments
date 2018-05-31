bd80f36acf179d2553f51684dd9ec52a7c2da53c
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-05-31 16:14:01 +0200

    Added snippet for comments.

--- reader.php
+++ reader.php
@@ -6,20 +6,43 @@ require_once 'FileHandler.php';
 $articleId = 1;
 $filePath = __DIR__ . '/' . $articleId . '.json';
 $file = FileHandler::readFile($filePath);
-$aComments = JsonHandler::readJson($file);
+$comments = JsonHandler::readJson($file);
 
-print_r($aComments);
-
-foreach ($aComments as $key => $comment) {
-    echo $key . ': ' . $comment . '<br>';
-}
-
-echo '<br><br>###################<br><br>';
-
-foreach ($aComments as $comment) {
+foreach ($comments as $comment) {
     ?>
-    <div>
-        <span><?= $comment['userName']; ?></span>
-    </div>
+    <section class="comment">
+        <div class="container">
+            <div class="box">
+                <article class="media">
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong><?= $comment->userName; ?></strong> 
+                                <?php
+                                if ($comment->userEmailAddress != '') {
+                                    ?>
+                                    <small><?= $comment->userEmailAddress; ?></small> 
+                                    <?php
+                                }
+                                ?>
+                                <small><?= date("d.m.Y h:i", $comment->timestampWritten); ?></small>
+                                <br>
+                                <?= $comment->comment; ?>
+                            </p>
+                        </div>
+                        <nav class="level is-mobile">
+                            <div class="level-left">
+                                <a class="level-item" aria-label="like">
+                                    <span class="icon is-small">
+                                        <i class="icon-heart" aria-hidden="true"></i>
+                                    </span>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </section>
     <?php
 }
