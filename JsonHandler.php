671e8a9b1d80686593fc825c7ead375ed6cb3bde
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-05-31 07:28:27 +0200

    Added decode functionality.

--- JsonHandler.php
+++ JsonHandler.php
@@ -1,10 +1,12 @@
 <?php
 class JsonHandler {
 
+    public function readJson($toRead) {
+        return json_decode($toRead, true);
+    }
+    
     public function writeJson($toWrite) {
         return json_encode($toWrite);
     }
-    
-    //readJson
 
 }
