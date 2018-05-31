9bf6a28dcacae1059b6841a92a3d684ff6ebcd22
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-05-31 07:26:55 +0200

    Packed router into method.

--- Comments.php
+++ Comments.php
@@ -1,14 +1,13 @@
 <?php
 class Comments {
     
-    
+    public function router() {
+        $kirby->set('route', array(
+            'pattern' => 'my/awesome/url',
+            'action'  => function() {
+                // do something here when the URL matches the pattern above
+            }
+        ));
+    }
     
 }
-
-
-$kirby->set('route', array(
-  'pattern' => 'my/awesome/url',
-  'action'  => function() {
-    // do something here when the URL matches the pattern above
-  }
-));
