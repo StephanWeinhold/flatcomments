67bb8d9ac87b609301bc0bad1f15679f6ec9f9e7
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-06-01 09:21:51 +0000

    Made methods static. // Added params to filesave. // Removed append to file because whole JSON will be written.

--- FileHandler.php
+++ FileHandler.php
@@ -1,18 +1,18 @@
 <?php
 class FileHandler {
 
-    public function readFile($filePath) {
+    public static function readFile($filePath) {
         $lines = file_get_contents($filePath);
-        
+
         if ($lines === false) {
             throw new Exception('JSON file couldn\'t be read.', 500);
         }
-        
+
         return $lines;
     }
 
-    public function writeFile($filePath, $json) {
-        if (!file_put_contents($filePath, $json.PHP_EOL , FILE_APPEND | LOCK_EX)) {
+    public static function writeFile($filePath, $json) {
+        if (!file_put_contents($filePath, $json.PHP_EOL , LOCK_EX | JSON_PRETTY_PRINT | JSON_FORCE_OBJECT)) {
             throw new Exception('JSON couldn\'t be written to file.', 500);
         }
     }
