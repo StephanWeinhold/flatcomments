ecc0abb54f378519b105224843ea2b4d13fbbd0f
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-06-01 21:52:30 +0200

    Show only unlocked comments.

--- getcomments.php
+++ getcomments.php
@@ -5,38 +5,40 @@ require_once 'Comments.php';
 <div class="container">
     <?php
     foreach (array_reverse(Comments::getComments($_GET['articleId'])) as $comment) {
-        ?>
-        <div class="box">
-            <article id="cmmnt_<?= $comment->id; ?>" class="media">
-                <div class="media-content">
-                    <div class="content">
-                        <p>
-                            <strong><?= $comment->userName; ?></strong>
-                            <?php
-                            if ($comment->userEmailAddress != '') {
-                                ?>
-                                <small><?= $comment->userEmailAddress; ?></small>
+        if ($comment->unlocked == true) {
+            ?>
+            <div class="box">
+                <article id="cmmnt_<?= $comment->id; ?>" class="media">
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong><?= $comment->userName; ?></strong>
                                 <?php
-                            }
-                            ?>
-                            <small><?= Comments::getTimeElapsed($comment->timestampWritten); ?></small>
-                            <br>
-                            <?= $comment->comment; ?>
-                        </p>
-                    </div>
-                    <nav class="level is-mobile">
-                        <div class="level-left">
-                            <a class="level-item" aria-label="like">
-                                <span class="icon is-small">
-                                    <i class="icon-heart" aria-hidden="true"></i>
-                                </span>
-                            </a>
+                                if ($comment->userEmailAddress != '') {
+                                    ?>
+                                    <small><?= $comment->userEmailAddress; ?></small>
+                                    <?php
+                                }
+                                ?>
+                                <small><?= Comments::getTimeElapsed($comment->timestampWritten); ?></small>
+                                <br>
+                                <?= $comment->comment; ?>
+                            </p>
                         </div>
-                    </nav>
-                </div>
-            </article>
-        </div>
-        <?php
+                        <nav class="level is-mobile">
+                            <div class="level-left">
+                                <a class="level-item" aria-label="like">
+                                    <span class="icon is-small">
+                                        <i class="icon-heart" aria-hidden="true"></i>
+                                    </span>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </article>
+            </div>
+            <?php
+        }
     }
     ?>
 </div>
