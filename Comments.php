1dfef7df482a46f5345a039a4e28507b7f75c174
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-06-04 14:11:16 +0000

    Save comment via AJAX.

--- Comments.php
+++ Comments.php
@@ -4,13 +4,12 @@ require_once 'FileHandler.php';
 
 class Comments {
 
-    public function router() {
-        $kirby->set('route', array(
-            'pattern' => 'my/awesome/url',
-            'action'  => function() {
-                // do something here when the URL matches the pattern above
-            }
-        ));
+    public static function getParam($key) {
+        if (array_key_exists($key, $_GET)) {
+            return $_GET[$key];
+        }
+
+        return false;
     }
 
     public static function checkForComments($articleId) {
@@ -26,52 +25,52 @@ class Comments {
     public static function getComments($articleId, $filterUnlocked = true) {
         $file = FileHandler::readFile(__DIR__ . '/' . $articleId . '.json');
         $comments = JsonHandler::readJson($file, false);
-        
+
         if ($filterUnlocked === true) {
             $comments = Comments::filterUnlockedComments($comments);
         }
-        
+
         return $comments;
     }
-    
+
     private static function filterUnlockedComments($commentsRaw) {
         $comments = [];
-        
+
         foreach ($commentsRaw as $comment) {
             if ($comment->unlocked == true) {
                 array_push($comments, $comment);
             }
         }
-        
+
         return $comments;
     }
-    
+
     public static function getTimeElapsed($timestamp, $level = 6) {
         $date = new DateTime();
         $date->setTimestamp($timestamp);
         $date = $date->diff(new DateTime());
-        
+
         $timeElapsed = json_decode($date->format('{"year":%y,"month":%m,"day":%d,"hour":%h,"minute":%i}'), true);
-        
+
         if ($timeElapsed['year'] > 0 || $timeElapsed['month'] > 5) {
             return date("d.m.Y h:i", $timestamp);
         }
-        
-        $timeElapsed = array_filter($timeElapsed);        
+
+        $timeElapsed = array_filter($timeElapsed);
         $timeElapsed = array_slice($timeElapsed, 0, $level);
-                
+
         $lastKey = key(array_slice($timeElapsed, -1, 1, true));
         $output = '';
-        
+
         foreach ($timeElapsed as $timeUnit => $value) {
             if ($output) {
                 $output .= $timeUnit != $lastKey ? ', ' : ' ' . 'and' . ' ';
             }
-            
+
             $timeUnit .= $value > 1 ? 's' : '';
             $output .= $value . ' ' . $timeUnit;
         }
-        
+
         return $output;
     }
 
