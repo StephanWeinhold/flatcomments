e01aa4b917e1650312158348598ae45cf5105c17
Stephan Weinhold <stephan.weinhold@gmail.com>
2018-06-04 14:12:16 +0000

    Save comment via AJAX.

--- caller.php
+++ caller.php
@@ -4,12 +4,14 @@ require_once 'Comments.php';
 require_once 'JsonHandler.php';
 require_once 'FileHandler.php';
 
-$articleId = 1;
-$userName = 'Stephan Weinhold';
-$userEmailAddress = 's@b.com';
-$comment = 'Dies ist ein Kommentar.';
+$articleId = Comments::getParam('articleId');
 
-$oComment = Comment::buildCommentFromPost($articleId, $userName, $userEmailAddress, $comment);
+$oComment = Comment::buildCommentFromPost(
+    $articleId,
+    Comments::getParam('userName'),
+    Comments::getParam('userEmailAddress'),
+    Comments::getParam('comment')
+);
 $existingComments = Comments::checkForComments($articleId);
 array_push($existingComments, $oComment);
 $jsonComment = JsonHandler::writeJson($existingComments);
